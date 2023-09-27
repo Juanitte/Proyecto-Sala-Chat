38430ed17286d76125e2c5a83052b21b0447beca
Juanite <juan13_barsa@hotmail.com>
2023-09-27 11:17:22 +0200

    modified model and xmlmanager

--- src/main/java/com/juanite/model/domain/Chat.java
+++ src/main/java/com/juanite/model/domain/Chat.java
@@ -3,27 +3,19 @@ package com.juanite.model.domain;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @XmlRootElement(name="Chat")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Chat {
     private String  name;
-    private List<User> users;
-    private List<Message> messages;
 
     public Chat() {
         this.name = "";
-        this.users = new ArrayList<>();
-        this.messages = new ArrayList<>();
     }
 
-    public Chat(String name, List<User> users, List<Message> messages) {
+    public Chat(String name) {
         this.name = name;
-        this.users = users;
-        this.messages = messages;
     }
 
     public String getName() {
@@ -34,21 +26,6 @@ public class Chat {
         this.name = name;
     }
 
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
-    public List<Message> getMessages() {
-        return messages;
-    }
-
-    public void setMessages(List<Message> messages) {
-        this.messages = messages;
-    }
 
     @Override
     public boolean equals(Object o) {

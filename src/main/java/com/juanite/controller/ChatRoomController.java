d3270145f268e7ede52b987cc44de6dab3b68e2f
Juanite <juan13_barsa@hotmail.com>
2023-10-03 13:43:09 +0200

    object input and output stream used

--- src/main/java/com/juanite/controller/ChatRoomController.java
+++ src/main/java/com/juanite/controller/ChatRoomController.java
@@ -1,9 +1,14 @@
 package com.juanite.controller;
 
 import com.juanite.App;
+import com.juanite.model.domain.Message;
+import com.juanite.util.AppData;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -12,17 +17,29 @@ import java.io.IOException;
 public class ChatRoomController {
 
     @FXML
-    public VBox vbx_roomList;
+    public Button btn_back;
     @FXML
-    public HBox hbx_room;
+    public TextArea txtarea_chatPanel;
+    @FXML
+    public TextField txtfld_msg;
+    @FXML
+    public Button btn_send;
     @FXML
     public Label lbl_roomName;
-    @FXML
-    public Button btn_enter;
 
+    public void initialize() {
+        lbl_roomName.setText(AppData.getCurrentRoom().getName());
+    }
     @FXML
-    private void backToMainMenu() throws IOException {
+    private void backToRoomsMenu() throws IOException {
+        Message nullMsg = null;
+        AppData.getCc().getOut().writeObject(nullMsg);
+
+        AppData.setCurrentRoom(null);
         App.setWindowSize(358, 291);
-        App.setRoot("home");
+        App.setRoot("navigation");
+    }
+
+    public void sendMessage() {
     }
 }
